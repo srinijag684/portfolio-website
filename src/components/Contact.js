3ad95351011b6b85d6098eafade3de660cc98b32
srinija684 <75112894+srinijag684@users.noreply.github.com>
2023-02-06 19:21:19 +0800

    Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,91 @@
 import { useState } from "react";
+import { Container,Row,Col } from "react-bootstrap";
+import contactImg from "../assets/img/contact-img.svg"
 
 function Contact(){
-    return(
+    const formInitialDetails = {
+        firstName: "",
+        lastName: "",
+        phone: "",
+        emailAddress: "",
+        message: ""
+    }
 
+    const [formDetails, setFormDetails]= useState(formInitialDetails);
+    const [buttonText, setButtonText] = useState('Send')
+    const [status, setStatus] = useState({})
+
+    function onFormUpdate(category,value){
+        setFormDetails({
+            ...formDetails,
+            [category]: value
+        })
+    }
+
+    function handleSubmit(){
+        
+    }
+
+    return(
+        <section className="contact" id="connect">
+            <Container>
+                <Row className="align-items-center">
+                    <Col md = {6}>
+                        <img src={contactImg} alt="Contact"/>
+                    </Col>
+                    <Col md = {6}>
+                        <h2>Get In Touch</h2>
+                        <form onSubmit={handleSubmit}>
+                            <Row>
+                                <Col sm={12} className="px-1">
+                                    <input type="text" 
+                                    value={formDetails.firstName} 
+                                    placeholder="First Name" 
+                                    onChange={(e)=>onFormUpdate('firstName',e.target.value)}
+                                    />
+                                </Col>
+                                <Col sm={12} className="px-1">
+                                    <input type="text" 
+                                    value={formDetails.firstName} 
+                                    placeholder="Last Name" 
+                                    onChange={(e)=>onFormUpdate('lastName',e.target.value)}
+                                    />
+                                </Col>
+                                <Col sm={12} className="px-1">
+                                    <input type="text" 
+                                    value={formDetails.firstName} 
+                                    placeholder="Phone Number" 
+                                    onChange={(e)=>onFormUpdate('phone',e.target.value)}
+                                    />
+                                </Col>
+                                <Col sm={12} className="px-1">
+                                    <input type="text" 
+                                    value={formDetails.firstName} 
+                                    placeholder="Email Address" 
+                                    onChange={(e)=>onFormUpdate('emailAddress',e.target.value)}
+                                    />
+                                </Col>
+                                <Col sm={12} className="px-1">
+                                    <textarea
+                                     rows="6" 
+                                     value={formDetails.message} 
+                                     placeholder="Message" 
+                                     onChange={(e) => onFormUpdate('message', e.target.value)}
+                                     />
+                                    <button type="submit"><span>{buttonText}</span></button>
+                                </Col>
+                                {
+                                    status.message && 
+                                    <Col>
+                                        <p className={status.success === false ?  "danger" : "success"}>{status.message}x</p>
+                                    </Col>
+                                }
+                            </Row>
+                        </form>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     );
 
 }
